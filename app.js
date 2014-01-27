7e49ce86e382238d0afe05ba8061e5c009a1b767
Miika Haapalainen <miika@Andreass-MacBook-Pro.local>
2014-01-26 21:12:44 -0800

    Adding some user data to mongo

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set('views', __dirname + '/html');
 
 app.use('/js', express.static('js'));
 app.use('/css', express.static('css'));
+app.use(express.bodyParser());
 
 // Swig will cache templates for you, but you can disable
 // that and use Express's caching instead, if you like:
@@ -23,6 +24,34 @@ app.get('/', function (req, res) {
 
 app.get('/signup', function (req, res) {
 	res.render('signup', { });
+});
+
+app.post('/api/register', function (req, res) {
+	var crypto = require('crypto');
+	var MongoClient = require('mongodb').MongoClient;
+
+	MongoClient.connect('mongodb://localhost/space', function(err, db) {
+		if(err) throw err;
+		
+		var collection = db.collection('users');
+		collection.find( { $or: [ { 'username' : req.body.username }, { 'email' : req.body.email } ] }).toArray(function(err, docs) {
+			if (docs.length > 0) {
+				console.log('User already exists');
+			} else {
+				var hash = crypto.createHash('md5').update(req.body.pwd).digest('hex');
+				var user = {
+					username : req.body.username,
+					email : req.body.email,
+					password : hash
+				};
+
+				db.collection('users').insert(user, function(err, records) {
+					if (err) throw err;
+					console.log("Record added as "+records[0]._id);
+				});
+			}
+        });
+	})
 });
 
 app.listen(3000);
