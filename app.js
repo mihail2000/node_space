dae25974483fa59ca61d3fe0f6f073c4a015fc47
Miika Haapalainen <miika.haapalainen@gmail.com>
2014-02-25 22:16:40 -0800

    Beatifulizing app.js and fixing routes.

--- app.js
+++ app.js
@@ -1,82 +1,114 @@
 /**
 	Entry point for the application
+
+	QC represents the top level object
 */
 
-/**
- * Module dependencies.
- */
-var express = require('express');
-var app = express();
-var swig  = require('swig');
-var MongoStore = require('connect-mongo')(express);
-var MatuFW = {};
-/**
- * Initializing all necessary objects
- */
+var QC = {
+	app : null,
+	config : {
+		server_version: "0.0.1",
+		server_name: "Dev Server",
+		port: 3000,
+		valid_routes: "/signin|/signup|/game|/logout|/starsystem|/lobby"
+	},
+	init: function () {
+		var express = require('express');
+		QC.app = express();
+		var swig  = require('swig');
+		var MongoStore = require('connect-mongo')(express);
 
-app.engine('html', swig.renderFile);
-app.use(express.bodyParser());
-app.set('view engine', 'html');
-app.set('views', __dirname + '/application/html');
-app.use('/public', express.static(__dirname + '/public'));
-app.use(express.cookieParser());
-app.use(express.session( { 
-	secret : 'lHDS4bObe68QtAnhZ9gRYOrZof3wGUkXZWwPUs9EABY63HbWGh0YYZ8mxRItsdX6ralOpN6XIB9NNbmeBDdHHfnWM0VzDkmt8shG' ,
-	store: new MongoStore({
-			db: 'space'
-	}),
-	cookie : {
-		maxAge : 100000000 }
-	})
-);
+		// Initializing all necessary objects
+		QC.app.engine('html', swig.renderFile);
+		QC.app.use(express.bodyParser());
+		QC.app.set('view engine', 'html');
 
-/**
-	setTemplateVariables
+		// Initialize public directories and views
+		QC.app.set('views', __dirname + '/application/html');
+		QC.app.use('/public', express.static(__dirname + '/public'));
 
-	Helper function to make certain session objects / variables available to swig temlpates.
-	Whenever there is a need for global access of variables in templates, add them in here.
+		// Initialize session handler
+		QC.app.use(express.cookieParser());
+		QC.app.use(express.session( { 
+			secret : 'lHDS4bObe68QtAnhZ9gRYOrZof3wGUkXZWwPUs9EABY63HbWGh0YYZ8mxRItsdX6ralOpN6XIB9NNbmeBDdHHfnWM0VzDkmt8shG' ,
+			store: new MongoStore({
+					db: 'space'
+			}),
+			cookie : {
+				maxAge : 100000000 }
+			})
+		);
 
-	TODO: Should move this into some sort of helper module and add some sort of configuration array to pass to the function.
-	e.g. 
-	var config = [
-		'current_user' : 'req.session.user',
-		'gameid' : 'req.session.gameid'
-	]
 
-*/
-setTemplateVariables = function(req) {
-	if (req.session.user != null) {
-		app.locals.current_user = req.session.user;
-	} else {
-		delete app.locals.current_user;
+		// Enable / disable view cache
+		// !!! NOTE: Don't leave both of these to `false` in production! !!!
+		// !!! NOTE2: Don't set both to TRUE either :)
+
+		// Enable / disable Express.js's cache
+		QC.app.set('view cache', false);
+		// Enable / disable Swig's cache
+		swig.setDefaults({ cache: false });
+
+		QC.app.get('/', function(req, res) {
+			if (req.session.user !== null) {
+				res.redirect('/lobby');
+			} else {
+				QC.setTemplateVariables(req);
+				var core_controller = require(__dirname + '/application/controller/core.js');
+				core_controller.route_get(req, res);				
+			}
+		});
+
+		// Route all needed HTTP GET requests through here
+		QC.app.get(QC.config.valid_routes, function (req, res) {
+			// TODO: There is something weird happening with this freaking system. This currently matches all possible routes.
+			QC.setTemplateVariables(req);
+			var core_controller = require(__dirname + '/application/controller/core.js');
+			core_controller.route_get(req, res);
+		});
+
+		// Route all API requests here
+		QC.app.post('/api/*', function (req, res) {
+			// Find the API call name
+			var core_controller = require(__dirname + '/application/controller/core.js');
+			core_controller.route_api(req, res);
+		});
+
+		// Launch the app
+		// TODO: Read the port from a config file
+		QC.app.listen(QC.config.port);
+		console.log('----------------------');
+		console.log('QuadCluster server ' + ' version: ' + QC.config.server_version);
+		console.log('Server name : ' + QC.config.server_name);
+		console.log('Listening on port ' + QC.config.port);
+	},
+
+	/**
+		setTemplateVariables
+
+		Helper function to make certain session objects / variables available to swig temlpates.
+		Whenever there is a need for global access of variables in templates, add them in here.
+
+		TODO: Should move this into some sort of helper module and add some sort of configuration array to pass to the function.
+		e.g. 
+		var config = [
+			'current_user' : 'req.session.user',
+			'gameid' : 'req.session.gameid'
+		]
+
+	*/
+	setTemplateVariables: function(req) {
+		if (req.session.user != null) {
+			QC.app.locals.current_user = req.session.user;
+		} else {
+			delete QC.app.locals.current_user;
+		}
+		if (req.session.gameid != null) {
+			QC.app.locals.gameid = req.session.gameid;
+		} else {
+			delete QC.app.locals.gameid;
+		}
 	}
-	if (req.session.gameid != null) {
-		app.locals.gameid = req.session.gameid;
-	} else {
-		delete app.locals.gameid;
-	}
-}
+};
 
-// Swig will cache templates for you, but you can disable
-// that and use Express's caching instead, if you like:
-app.set('view cache', false);
-// To disable Swig's cache, do the following:
-swig.setDefaults({ cache: false });
-// NOTE: You should always cache templates in a production environment.
-// Don't leave both of these to `false` in production!
-
-// TODO: There is something weird happening with this freaking system. This currently matches all possible routes.
-app.get('/|/signin|/signup|/game|/logout|/starsystem|/lobby', function (req, res) {
-	setTemplateVariables(req);
-	var core_controller = require(__dirname + '/application/controller/core.js');
-	core_controller.route_get(req, res);
-});
-
-app.post('/api/*', function (req, res) {
-	// Find the API call name
-	var core_controller = require(__dirname + '/application/controller/core.js');
-	core_controller.route_api(req, res);
-});
-
-app.listen(3000);
-console.log('Listening on port 3000');
+QC.init();
